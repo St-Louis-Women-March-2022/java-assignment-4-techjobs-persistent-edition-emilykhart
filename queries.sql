098e57950ae30cd385de4d5c3c27ab974b7aaed3
Emily Hart <emilykhart726@gmail.com>
2022-11-21 19:31:54 -0600

    Part 1 and 2 Done

--- queries.sql
+++ queries.sql
@@ -1,6 +1,17 @@
 -- Part 1: Test it with SQL
+Table: job
+Columns:
+id int
+employer varchar(255)
+name varchar(255)
+skills varchar(255)
 
 -- Part 2: Test it with SQL
+
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
+
 
 -- Part 3: Test it with SQL
 
